1a14d5d06f91116ee0d1d472e1bb926e7bad4e1c
Dr. Sathiyaraj Thangasamy <78794135+neyakkoot@users.noreply.github.com>
2026-01-28 20:51:23 +0530

    Create common.js

---
+++ Neyakkoo/common.js
@@ -0,0 +1,2 @@
+Tool 1_Picture_Tag_Button:- mw.loader.load( '/w/index.php?title=User:Neyakkoo/engpicturebutton.js&action=raw&ctype=text/javascript' );
+Tool 2_Multicolbutton:- mw.loader.load( '/w/index.php?title=User:Neyakkoo/Multicolbutton.js&action=raw&ctype=text/javascript' );
