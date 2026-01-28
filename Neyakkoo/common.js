a223e74790816300b2923d2f24b2179334c44098
Dr. Sathiyaraj Thangasamy <78794135+neyakkoot@users.noreply.github.com>
2026-01-28 21:25:37 +0530

    Update common.js

--- Neyakkoo/common.js
+++ Neyakkoo/common.js
@@ -1,2 +1,10 @@
-Tool 1_Picture_Tag_Button:- mw.loader.load( '/w/index.php?title=User:Neyakkoo/engpicturebutton.js&action=raw&ctype=text/javascript' );
-Tool 2_Multicolbutton:- mw.loader.load( '/w/index.php?title=User:Neyakkoo/Multicolbutton.js&action=raw&ctype=text/javascript' );
+/*Tool 1_Picture_Tag_Button:- */
+  mw.loader.load( '/w/index.php?title=User:Neyakkoo/engpicturebutton.js&action=raw&ctype=text/javascript' );
+/*Tool 2_Multicolbutton:- */ 
+  mw.loader.load( '/w/index.php?title=User:Neyakkoo/Multicolbutton.js&action=raw&ctype=text/javascript' );
+/**
+ * EN2TA Infobox கருவியை அழைப்பதற்கான நிரல்
+ * உருவாக்குபவர்: பயனர்:Neyakkoo
+ */
+/**Tool 3_EN2TA_Infobox*/
+mw.loader.load('//ta.wikipedia.org/w/index.php?title=User:Neyakkoo/EN2TA_Infobox.js&action=raw&ctype=text/javascript');
